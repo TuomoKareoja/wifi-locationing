b7e8eb02f8203ed9ef5cc89cadf85e5c9d8e01b8
Tuomo Kareoja <tuomo.kareoja@gmail.com>
2019-11-28 16:11:08 +0100

    preprocessing for competition data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -17,14 +17,19 @@ def main():
     logger.info("Loading the datasets")
     df_train = pd.read_csv(os.path.join("data", "raw", "trainingData.csv"))
     df_test = pd.read_csv(os.path.join("data", "raw", "validationData.csv"))
+    df_comp = pd.read_csv(os.path.join("data", "raw", "competitionData.csv"))
 
     logger.info("Combining the datasets")
     df_train["train"] = True
     df_test["train"] = False
-    df = pd.concat([df_train, df_test], ignore_index=True)
+    df_comp["train"] = False
+    df_train["comp"] = False
+    df_test["comp"] = False
+    df_comp["comp"] = True
+    df = pd.concat([df_train, df_test, df_comp], ignore_index=True)
 
-    logger.info("Removing duplicates (some in the training set)")
-    df = df[~df.duplicated()]
+    logger.info("Removing duplicates (some in the training and competition sets)")
+    df = df[(~df.duplicated()) | (df.comp)]
 
     logger.info(
         "Dropping unnecessary columns not available in the validation set (all null)"
@@ -36,16 +41,21 @@ def main():
     df.columns = [column.lower() for column in df.columns]
 
     logger.info(
-        "Changing missing values of WAP column from 100 to -110 (weaker than all signals)"
+        "Changing missing of WAP columns from 100 to -110 (weaker than all signals)"
     )
     wap_columns = [column for column in df.columns if "wap" in column]
     df[wap_columns] = df[wap_columns].replace(100, -110)
 
     logger.info("Saving to data/processed")
-    df_train = df[df.train]
-    df_test = df[~df.train]
+    df_train = df[df["train"]].copy()
+    df_test = df[(~df["train"]) & ~(df["comp"])].copy()
+    df_comp = df[df["comp"]].copy()
+    df_train.drop(columns=["comp"], inplace=True)
+    df_test.drop(columns=["comp"], inplace=True)
+    df_comp.drop(columns=["comp", "train"], inplace=True)
     df_train.to_csv(os.path.join("data", "processed", "train.csv"), index=False)
     df_test.to_csv(os.path.join("data", "processed", "test.csv"), index=False)
+    df_comp.to_csv(os.path.join("data", "processed", "comp.csv"), index=False)
 
 
 if __name__ == "__main__":
